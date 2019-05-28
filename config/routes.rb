2a66d3a6793d99dc44c773f68317c381a782465c
rudyNK <rudy@noukod.com>
2019-05-27 22:02:48 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,8 @@ resources :articles do
 end
   
   root 'welcome#index'
-
-
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
